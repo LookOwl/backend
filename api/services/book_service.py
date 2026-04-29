fc963eaa0013c726e5dff5d27763f8c3dd31255a
bxcowo <a.flores.a@uni.pe>
2026-04-29 00:49:03 -0500

    LOOKOWL-57 style: Cambiar el nombre del directorio de models a domain.

--- api/services/book_service.py
+++ api/services/book_service.py
@@ -1,11 +1,11 @@
-from models.book import Book
+from domain.book import Book
 from api.dtos.book_dto import RegisterBookDto
 
 
 class BookService:
     def __init__(self, book_repo) -> None:
         self.repo = book_repo
-        
+
 
     def registerBook(self, book : RegisterBookDto):
         toCreate = Book(
