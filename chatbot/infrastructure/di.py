b5c2c6f6c2c997935b54547ea4d85ec972e27a5b
bxcowo <a.flores.a@uni.pe>
2026-06-30 11:02:45 -0500

    chore: Create dependency injection pattern to expose use cases

---
+++ chatbot/infrastructure/di.py
@@ -0,0 +1,48 @@
+from fastapi import Depends
+from anthropic import AsyncAnthropic
+from books.application.use_cases.get_book_recommendations import GetBookRecommendations
+from books.application.use_cases.get_query_recommendations import GetQueryRecommendations
+from books.application.use_cases.search_books import SearchBook
+from books.infrastructure.di import get_book_recommendations_uc, get_query_recommendations_uc, get_search_book_uc
+from chatbot.application.ports import LLMPort, TokenCounterPort, ToolExecutorPort
+from chatbot.application.use_cases.chat_orchestrator import ChatOrchestrator
+from chatbot.domain.conversation_repository import ConversationRepository
+from chatbot.infrastructure.adapters.anthropic_adapter import AnthropicAdapter
+from chatbot.infrastructure.adapters.book_tool_executor import BookToolExecutor
+from chatbot.infrastructure.adapters.sql_conversation_repository import SQLConversationRepository
+from chatbot.infrastructure.adapters.token_counter import TokenizerCounterAdapter
+
+
+def get_token_counter() -> TokenCounterPort:
+    return TokenizerCounterAdapter()
+
+def get_anthropic_llm() -> LLMPort:
+    client = AsyncAnthropic()
+    return AnthropicAdapter(client=client, model="deepseek-v4-flash")
+
+def get_book_tool_executor(
+    search_books_uc: SearchBook = Depends(get_search_book_uc),
+    book_recommendations_uc: GetBookRecommendations = Depends(get_book_recommendations_uc),
+    query_recommendations_uc: GetQueryRecommendations = Depends(get_query_recommendations_uc)
+) -> ToolExecutorPort:
+    return BookToolExecutor(
+        search_books_uc=search_books_uc,
+        book_recommendations_uc=book_recommendations_uc,
+        query_recommendations_uc=query_recommendations_uc,
+    )
+
+def get_conversation_repo() -> ConversationRepository:
+    return SQLConversationRepository()
+
+def get_chat_orchestrator(
+    repo: ConversationRepository = Depends(get_conversation_repo),
+    llm: LLMPort = Depends(get_anthropic_llm),
+    token_counter: TokenCounterPort = Depends(get_token_counter),
+    tool_executor: ToolExecutorPort = Depends(get_book_tool_executor),
+) -> ChatOrchestrator:
+    return ChatOrchestrator(
+        repo=repo,
+        llm=llm,
+        token_counter=token_counter,
+        tool_executor=tool_executor,
+    )
